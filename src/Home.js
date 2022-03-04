3a9776cee4625c733fbb78c8e67914659930c99d
Viktor <63589696+Viktor-hash@users.noreply.github.com>
2022-03-05 00:35:50 +0100

    Website_valpha

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,28 @@
-import img_cover from "../images/img_cover2.jpeg";
+import img_cover from "../images/move_study_website.jpg";
 import FadeIn from "react-fade-in";
-
+import React from "react";
 const Home = () => {
+  React.useEffect(() => {
+    document.body.style.backgroundColor = "#f1eaf2";
+  }, []);
+
+  const OnTitleClicked = () => {};
+
   return (
     <FadeIn>
-      <div className="home">
-        <img className="img_cover" src={img_cover} alt="" />
-      </div>
+      <h1 className="Title" onClick={OnTitleClicked}>
+        <span>Victor Perrault</span>
+        <h2 className="About" onClick={OnTitleClicked}>
+          <span>ABOUT</span>
+        </h2>
+        <h2 className="Work" onClick={OnTitleClicked}>
+          <span>WORK</span>
+        </h2>
+        <h2 className="Contact" onClick={OnTitleClicked}>
+          <span>CONTACT</span>
+        </h2>
+      </h1>
+      <img className="Image" src={img_cover} alt="" />
     </FadeIn>
   );
 };
