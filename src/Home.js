aa4c2d9aebbeb2f102bdb4130238a840a438f9b0
Viktor <63589696+Viktor-hash@users.noreply.github.com>
2022-03-07 12:03:33 +0100

    V1

--- src/Home.js
+++ src/Home.js
@@ -10,19 +10,34 @@ const Home = () => {
 
   return (
     <FadeIn>
-      <h1 className="Title" onClick={OnTitleClicked}>
-        <span>Victor Perrault</span>
-        <h2 className="About" onClick={OnTitleClicked}>
-          <span>ABOUT</span>
-        </h2>
-        <h2 className="Work" onClick={OnTitleClicked}>
-          <span>WORK</span>
-        </h2>
-        <h2 className="Contact" onClick={OnTitleClicked}>
-          <span>CONTACT</span>
-        </h2>
-      </h1>
-      <img className="Image" src={img_cover} alt="" />
+      <div className="nav">
+        <div className="nav_header">
+          <h1 className="Title" onClick={OnTitleClicked}>
+            <span>Victor Perrault</span>
+          </h1>
+        </div>
+        <div className="nav_list">
+          <h2 className="About" onClick={OnTitleClicked}>
+            <span>ABOUT</span>
+          </h2>
+          <h2 className="Work" onClick={OnTitleClicked}>
+            <span>WORK</span>
+          </h2>
+          <h2 className="Contact" onClick={OnTitleClicked}>
+            <span>CONTACT</span>
+          </h2>
+        </div>
+      </div>
+      <div className="main">
+        <div className="containerHome">
+          <img className="Image" src={img_cover} alt="" />
+        </div>
+      </div>
+      <div className="footer">
+        <div className="container">
+          <p>© Victor Perrault 2022. Currently in Paris</p>
+        </div>
+      </div>
     </FadeIn>
   );
 };
